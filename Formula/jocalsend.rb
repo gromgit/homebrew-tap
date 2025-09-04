a1717810967987a31f3783eedcea528f2f10a1f1
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-09-04 12:33:01 +0000

    jocalsend: add 1.61803398 bottle.

--- Formula/jocalsend.rb
+++ Formula/jocalsend.rb
@@ -9,6 +9,14 @@ class Jocalsend < Formula
     url "https://git.kittencollective.com/nebkor/joecalsend.git"
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/gromgit/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3369d81c01e430f72e3249b0e8581fd889e64ec79ea352e7a3ca18210e2eb97a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e9a27fa749303c6f71b3bad710cde5ab88ecca2cd4921ea32316f1ab7aade753"
+    sha256 cellar: :any_skip_relocation, ventura:       "1de30fb9a7ede1d0f050588ea84313acb91b29b438b07164823fa6f7ecdae37a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8a2d8f7f4b8576a4305aaadd19fe52b62e4e57f2587b371a84878d3aaeebe997"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
