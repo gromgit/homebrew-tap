1fb64c1696be680beafba6859bccdce6f861e2af
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-09-22 04:58:15 +0000

    telegram-owl: add 1.3.2 bottle.

--- Formula/telegram-owl.rb
+++ Formula/telegram-owl.rb
@@ -5,6 +5,14 @@ class TelegramOwl < Formula
   sha256 "52f73a7c6302c6dd5f71dbf333362aa73476093efdbdec6bba07b14a13dc3ff6"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/gromgit/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe2add4415be0a624a6476e74df132293b760689a24139048f69abc6db6cae32"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "14d78f381f8e33d8d0c28a2b25555c457a292cdaefb38ac6dc1bb0fa6a784255"
+    sha256 cellar: :any_skip_relocation, ventura:       "02ec2679204ade115bdc7450441dc330905bb3eddaf36b524519247e78398607"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "04c8efee62174b4be90642b65aea5b6eb4d7e49de4844ccf403f2ebef29cbebb"
+  end
+
   depends_on "go" => :build
 
   def install
