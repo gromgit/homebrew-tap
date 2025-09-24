b89513d04c7f4dfd5d751d73c068b55d123a3f80
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-09-24 00:46:47 +0000

    telegram-owl: update 1.3.3 bottle.

--- Formula/telegram-owl.rb
+++ Formula/telegram-owl.rb
@@ -7,10 +7,10 @@ class TelegramOwl < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/gromgit/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe2add4415be0a624a6476e74df132293b760689a24139048f69abc6db6cae32"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "14d78f381f8e33d8d0c28a2b25555c457a292cdaefb38ac6dc1bb0fa6a784255"
-    sha256 cellar: :any_skip_relocation, ventura:       "02ec2679204ade115bdc7450441dc330905bb3eddaf36b524519247e78398607"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "04c8efee62174b4be90642b65aea5b6eb4d7e49de4844ccf403f2ebef29cbebb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e38a0ed5b1155f3ab7942b1e36267c75adb51e2c82664e20e7572115e2f169ab"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f2888167233a1b54f7b435faa3c023edc603270a4d59d82bfe21a6f43020c543"
+    sha256 cellar: :any_skip_relocation, ventura:       "0afbb93197cc7826a34726c30dc6d0600103baaff1f873eff8e266f88a7c531b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f5453cc3bb60fa0258c119950914bb7b0ce85d22eca58ce12bc2b478d3e1d581"
   end
 
   depends_on "go" => :build
