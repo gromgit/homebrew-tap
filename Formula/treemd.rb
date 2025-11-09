d50168a7cee30749394588bcfdfc7ce677dae56e
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-11-09 10:20:08 +0000

    treemd: add 0.1.2 bottle.

--- Formula/treemd.rb
+++ Formula/treemd.rb
@@ -5,6 +5,14 @@ class Treemd < Formula
   sha256 "7ea812e2fa692faddd04eaf3d77fa71fea1a252374359fe9b1a88b56f075682f"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/gromgit/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f7d270faa6a19e11828eeb218dc878ad72f3468543234cea01e55b6434efbfa4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f2e124fbd0d42b9a23b64a4b41989cbf68f33c1df162cc8aff2b4f59b3903d6c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ef012e30de93b0102629201e1dfd1b290723381aa88d29f9d4cc4a0198d3c0b7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "34dae004e8bb6a1d459075495c4c368893f8568171076fe1a2e8ec98c7d951b9"
+  end
+
   depends_on "rust" => :build
 
   def install
