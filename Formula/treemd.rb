02d5b9da6cf117e7ec36fcacc96a98d51a81321a
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-12-03 11:17:11 +0000

    treemd: update 0.4.2 bottle.

--- Formula/treemd.rb
+++ Formula/treemd.rb
@@ -7,10 +7,10 @@ class Treemd < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/gromgit/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8f6d856d0eb62957cbccebabb08f24c50ffd72a7f070aa0e373f9ddf73540c8f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8d11f0a94067f07164f3520cc72a739477488c5b6e02f47dfb44c0dcb765f0bc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "02f2a2f2ec61d5b4a21c8ec41607705f5d95d395f7f60e3a8958dbf88bc2d441"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2f3523f8b6f24cc5255c2c6dc3b4139bb7c94838b40187fe5094e94c1233aed4"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "460c49bcaed73c75bc3a7ca841759ba5b6095e5f1b95ddff7028f1890aec096a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5f4853881a93492a4fc5cc1a1d9bf2c1fa4b6c46b78cccb18e3f2dd177da1a43"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "332d30103c7e2744bfab84acea4d258e6a4f70e4893db0725a0cb3699fb4dc10"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2471953e8eb3e3ec0bf801e9ad564997353a309e3ec664daefbe2888febe7a56"
   end
 
   depends_on "rust" => :build
