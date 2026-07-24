dc9b2191f55902818ec69cbe865ac0230860d7d4
Adrian Ho <215702+gromgit@users.noreply.github.com>
2026-07-24 09:31:22 +0000

    mold-ai: update 0.20.2 bottle.

--- Formula/mold-ai.rb
+++ Formula/mold-ai.rb
@@ -7,10 +7,9 @@ class MoldAi < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/gromgit/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1ce53d0b75e2a097ca86a63b4c6a2d1ef458d7087e33bfc5a2123457eae62ad8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1cae360cb720a1432d9531936e6df69c0a25955c87b19d3d0e717baf42db9f52"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "62cdd351f4159ce9b539c8b57ba9b59558f01eedd8b22ac17e2e0a43aa46df3f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d8ed6847422f833f75f90c264d1b3008cd9202beeb6f3f3b3b779818ff47869"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "49a6e855f94c04a422aaa6cc16675fd89769c7acfe66a13694b96a7d69185b6b"
+    sha256 cellar: :any,                 arm64_linux:  "cd925a14f567a5bbfedde4a166ec1d8259689dd4a371c639f8725495ee1ba6fc"
+    sha256 cellar: :any,                 x86_64_linux: "580cac6cf3687c92707e6c33e9c2ea40f862b999d19a71d80c71bc473a7169bd"
   end
 
   depends_on "lld" => :build
